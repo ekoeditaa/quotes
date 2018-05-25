77083f116ef92fcc393e5f229ff75ada7bdb8ee1
ekoeditaa <ekoeditaa@gmail.com>
2018-05-25 14:53:25 +0800

    quote displayer component

---
+++ src/assets/components/Quote.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="col-auto quote">
+    <div class="" @click="removeQuote" style="width: 18rem;">
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <p class="card-text"><slot></slot></p>
+        </div>
+    </div>
+    </div>
+</div>
+    
+</template>
+
+<script>
+import { quoteBus } from "../../main";
+
+export default {
+    props: {
+        quote: String
+    },
+    methods: {
+        removeQuote(){
+            quoteBus.$emit('quoteDeleted', this.quote);
+        }
+    }
+};
+</script>
+
+<style scoped>
+.quote {
+    font-family: Arizonia;
+    font-size: 30px;
+    margin: 2%;
+    cursor: pointer;
+}
+</style>
