6d865e432cfdc20e3cb4a92f08eb4929717ba4a5
ekoeditaa <ekoeditaa@gmail.com>
2018-05-25 14:52:31 +0800

    add info bar at the bottom

---
+++ src/assets/components/Info.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="alert alert-primary mt-5" role="alert">
+        <div class="text-center"> Info: Click on a quote to delete it </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
+
