cf31977573ac8fe675aa6f09706c38077adbd592
ekoeditaa <ekoeditaa@gmail.com>
2018-05-25 14:54:45 +0800

    add app.vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <quotesAdded></quotesAdded>
+        <addQuote></addQuote>
+        <quotes></quotes>
+        <info></info>
+    </div>
+</template>
+
+<script>
+import ProgressBar from "./assets/components/ProgressBar.vue";
+import AddQuote from "./assets/components/AddQuote.vue";
+import Quote from "./assets/components/Quote.vue";
+import Quotes from "./assets/components/Quotes.vue";
+import QuotesAdded from "./assets/components/QuotesAdded.vue";
+import info from "./assets/components/Info.vue";
+
+    export default {
+        components: {
+            addQuote: AddQuote,
+            progressBar: ProgressBar,
+            quote: Quote,
+            quotes: Quotes,
+            quotesAdded: QuotesAdded,
+            info
+        }        
+    }
+</script>
+
+<style>
+</style>
