f0b09f0c34e92095982a6518777aca7740b93c50
ekoeditaa <ekoeditaa@gmail.com>
2018-05-25 14:53:45 +0800

    show all quotes

---
+++ src/assets/components/QuotesAdded.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="header">
+    <h2 class="top"> Quotes Added </h2>
+    <progress-bar></progress-bar>
+</div>
+
+    
+</template>
+
+<script>
+import ProgressBar from "./ProgressBar.vue";
+
+export default {
+    components: {
+        progressBar: ProgressBar
+    }
+}
+</script>
+
+<style scoped>
+.header {
+    margin: 5%;
+}
+.top {
+    font-family: 'Courier New', Courier, monospace;
+}
+
+</style>
